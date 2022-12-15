8782c71ac3e4155ba3f3482a408f0251e37ac07f
DmitriyM <mironowd697@gmail.com>
2022-12-15 18:08:38 +0600

    add .eslintrc.yml

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
-import  readlineSync  from 'readline-sync';
+import readlineSync from 'readline-sync';
 
-function startGame () {
-	const userName = readlineSync.question('May I have your name? ');
-	console.log(`Hello, ${userName}!`);
-	return userName;
-};
+function startGame() {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  return userName;
+}
 
 export default startGame;
