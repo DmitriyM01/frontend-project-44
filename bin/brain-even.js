821e9a031cca081078f2dc142bcfeeb9d45cb726
DmitriyM <mironowd697@gmail.com>
2022-12-16 18:51:26 +0600

    modified brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,30 +1,43 @@
 import readlineSync from 'readline-sync';
+
+function getNumber() {
+  const number = Math.floor(Math.random() * 101);
+  return number;
+};
+
+function evenOrNot(number) {
+  const rest = number % 2;
+  return rest;
+};
+
+function getRightAnswer(rest) {
+  if (rest === 1) {
+    rightAnswer = 'no';
+  } else {
+      rightAnswer = 'yes';
+  }
+};
+
 function findEven() {
   
+  const result;
   const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
   console.log(rule);
   
-  for (let i = 0; i < 3; i = i + 1) {
+  for (let i = 0; i < 3; i += 1) {
 
-    let num = Math.floor(Math.random() * 101);
-    let rest = num % 2;
-    let rightAnswer;
-    
-    if (rest === 1) {
-      rightAnswer = 'no';
-    } else {
-        rightAnswer = 'yes';
-    }
+    const num = getNumber()
+    const rest = evenOrNot(num);
+    const rightAnswer = getRightAnswer(rest);
 
     console.log(`Question: ${num}`);
     const userAnswer = readlineSync.question('Your answer: ');
-    if (userAnswer !== rightAnswer) {
-      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${rightAnswer}.`);
-      console.log('Let\'s try again, Bill!');
-      return false;
+    if (userAnswer === rightAnswer) {
+      result = true;
+    } else {
+      result = false;
+      return result;
     }
-    console.log('Correct!');
   }
-  return true;
 }
 export default findEven;
